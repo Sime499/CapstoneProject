30d232d0822f9de97cf05e28237f32966e1f96e0
Garton67 <50034619+Garton67@users.noreply.github.com>
2021-10-08 15:38:13 -0500

    finished with node/express

--- Backend/server.js
+++ Backend/server.js
@@ -4,12 +4,13 @@ import data from './data.js';
 const app = express();
 
 app.get('/api/products', (req, res) => {
-    res.send(data.prodcuts);
+    res.send(data.products);
 })
 
 app.get('/', (req, res) => {
     res.send('server is ready')
 })
-app.listen(5000, () => {
-    console.log('Serve at http://localhost:5000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
 });
