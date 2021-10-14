c68d821e402985891e713bfd0aca8cd6e9f51796
Garton67 <50034619+Garton67@users.noreply.github.com>
2021-10-14 11:19:53 -0500

    added change password

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/anjucoffee', {
