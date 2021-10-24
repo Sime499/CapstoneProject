1fda11cbaa685cda4540bb245e2428bdfe1da48a
Garton67 <50034619+Garton67@users.noreply.github.com>
2021-10-24 01:55:41 -0500

    push to heroku

--- FinalProject/src/Display/Footer.js
+++ FinalProject/src/Display/Footer.js
@@ -1,53 +1,61 @@
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
-export default function Footer() {
+export default function Footer(props) {
   return (
-    <footer className="Footer-Container">
-      <Box px={{ xs: 3, sm: 10 }} py={{ xs: 5, sm: 10 }} color="white">
-        <Container maxWidth="lg">
-          <Grid container spacing={5}>
-            <Grid item spacing xs={12} sm={4}>
-              <Box>Learn</Box>
-              <Box>
-                <Link to="/Learn">Our Story</Link>
-              </Box>
-              <Box>
-                <Link to="/Shop">Our Coffee</Link>
-              </Box>
-              <Box>
-                <Link to="/Contact">WholeSale</Link>
-              </Box>
-            </Grid>
-            <Grid item spacing xs={12} sm={4}>
-              <Box>Account</Box>
-              <Box>
-                <Link to="/Shop">SignIn</Link>
-              </Box>
-              <Box>
-                <Link to="/Contact">Contact</Link>
-              </Box>
-            </Grid>
-            <Grid item spacing xs={12} sm={4}>
-              <Box>Social Media</Box>
-              <Box>
-                <Link to="/Learn">Twitter</Link>
-              </Box>
-              <Box>
-                <Link to="/Shop">Facebook</Link>
-              </Box>
-              <Box>
-                <Link to="/Contact">Instagram</Link>
-              </Box>
-            </Grid>
-          </Grid>
-        </Container>
-        <Box className="footer">
-          Sime and Cory &reg;{new Date().getFullYear()}
-        </Box>
-      </Box>
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="footer-col">
+            <h4>Learn</h4>
+            <ul>
+              <li>
+                <Link to="Learn ">Our Coffee</Link>
+              </li>
+              <li>
+                <Link to="Learn">Our Story</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Account</h4>
+            <ul>
+              <li>
+                <Link to="/signIn">Sign In</Link>
+              </li>
+              <li>
+                <Link to="/Register">Register</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>customer</h4>
+            <ul>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/contact">WholeSale</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Follow ANJU Coffee</h4>
+            <div className="social-links">
+              <a href="https://facebook.com">
+                <i class="fab fa-facebook"></i>
+              </a>
+              <a href="https://twitter.com">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="https://www.instagram.com">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
